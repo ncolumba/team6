104add7eac5876d248763595fdd4d48bc339b42c
Nichelle Columba <ncolumba@terpmail.umd.edu>
2023-03-12 21:47:31 -0400

    Adding first code to info sys website.

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def hello():  # put application's code here
 def about():  # put application's code here
     return render_template('about.html')
 
+@app.route('/home')
+def home():  # put application's code here
+    return render_template('home.html')
+
 if __name__ == '__main__':
     app.run()
